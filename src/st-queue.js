cd5c260c0acfd5da3104aef45b75c512a2ba16a2
Tatsiana Bivoina <tanya.urainbow@mail.ru>
2021-10-18 20:29:17 +0300

    feat: add solution to Queue task

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 module.exports = class Queue {
-
+  constructor() {
+    this.first = null;
+    this.size = 0;
+    this.data = null;
+    this.next = null;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.first;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    let node = new ListNode(value);
+
+    if (!this.first){
+      this.first = node;
+    } else {
+      let n = this.first;
+      while (n.next) {
+        n = n.next;
+      }
+      n.next = node;
+    }
+
+    this.size += 1;
+    return node;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let temp = this.first.value;
+    this.first = this.first.next;
+    this.size -= 1;
+    return temp;
   }
 
 }
